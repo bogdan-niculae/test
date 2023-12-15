a0d69428185200e0eaafcea2f1fa5dd01a8c5241
barutbogdan <bogdan.barut@expertvision.ro>
2023-12-15 17:46:34 +0200

    first-commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,36 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+     // https://public-api.wordpress.com/wp/v2/sites/gatsbyjsexamplewordpress.wordpress.com/pages/
+    /*
+     * Gatsby's data processing layer begins with “source”
+     * plugins. Here the site sources its data from WordPress.
+     */
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
+         * Example : 'demo.wp-api.org' or 'www.example-site.com'
+         */
+        // baseUrl: `allbright.digital-extend.com`,
+          url: `http://allbright.digital-extend.com`,
+        // The protocol. This can be http or https.
+          // protocol: `https`,
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the assumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: false,
+        // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
+        // This feature is untested for sites hosted on WordPress.com
+        useACF: true,
+      },
+    },
+    /**
+     * The following plugins aren't required for gatsby-source-wordpress,
+     * but we need them so the default starter we installed above will keep working.
+     **/
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
